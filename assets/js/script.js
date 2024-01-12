d9e2695afad791c34c0f7f47cce8865becf66569
Vitto633 <vitor.santos633@etec.sp.gov.br>
2024-01-12 07:31:08 -0300

    fix: finalizada a primeira pagina

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,2 @@
-let ultimaPosicao = 0;
-
-document.addEventListener("scroll", function scrollHeader(){
-    const nav = document.querySelector("nav"); 
-    let atualPosicao = window.scrollY;
-
-    if(atualPosicao > ultimaPosicao) {
-        nav.style.top = '-5em'
-        TiraDropDown(dropDown);
-
-    } else {
-        nav.style.top = '0'
-        
-    }
-
-    ultimaPosicao = atualPosicao;
-
-})
+const footer = document.querySelector("#footer");
+footer.scrollIntoView;
