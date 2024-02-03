6e66ecd6cf0b86eb231f7158c0efa284a6b61c6e
Vitto633 <vitor.santos633@etec.sp.gov.br>
2024-02-03 12:24:52 -0300

    feat adicionado novos botões a página de tipos de financiamento

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,59 @@
 function mudaCard() {
   let card = document.querySelector('.btn-card');
+  let button = document.getElementById('btnText');
 
   if (card) {
     if(card.classList.contains('click')){
       card.classList.remove('click');
+      button.innerHTML = "+";
     }else{
       card.classList.add('click');
+      button.innerHTML = "-";
+    }
+    
+  }
+}
+
+function mudaCard2() {
+  let card = document.querySelector('.btn-card2');
+  let button = document.getElementById('btnText2');
+  if (card) {
+    if(card.classList.contains('click')){
+      card.classList.remove('click');
+      button.innerHTML = "+";
+    }else{
+      card.classList.add('click');
+      button.innerHTML = "-";
+    }
+    
+  }
+}
+
+function mudaCard3() {
+  let card = document.querySelector('.btn-card3');
+  let button = document.getElementById('btnText3');
+  if (card) {
+    if(card.classList.contains('click')){
+      card.classList.remove('click');
+      button.innerHTML = "+";
+    }else{
+      card.classList.add('click');
+      button.innerHTML = "-";
+    }
+    
+  }
+}
+
+function mudaCard4() {
+  let card = document.querySelector('.btn-card4');
+  let button = document.getElementById('btnText4');
+  if (card) {
+    if(card.classList.contains('click')){
+      card.classList.remove('click');
+      button.innerHTML = "+";
+    }else{
+      card.classList.add('click');
+      button.innerHTML = "-";
     }
     
   }
