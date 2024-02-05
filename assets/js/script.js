c4969a64c306678fe86f30336613bba40027d055
Vitto633 <vitor.santos633@etec.sp.gov.br>
2024-02-04 22:25:10 -0300

    feat: adicionado a ultima parte da segunda página

--- assets/js/script.js
+++ assets/js/script.js
@@ -58,3 +58,93 @@ function mudaCard4() {
     
   }
 }
+
+function mudaCard4() {
+  let card = document.querySelector('.btn-card4');
+  let button = document.getElementById('btnText4');
+  if (card) {
+    if(card.classList.contains('click')){
+      card.classList.remove('click');
+      button.innerHTML = "+";
+    }else{
+      card.classList.add('click');
+      button.innerHTML = "-";
+    }
+    
+  }
+}
+
+function mudaCard5() {
+  let card = document.querySelector('.btn-card5');
+  let button = document.getElementById('btnText5');
+  if (card) {
+    if(card.classList.contains('click')){
+      card.classList.remove('click');
+      button.innerHTML = "+";
+    }else{
+      card.classList.add('click');
+      button.innerHTML = "-";
+    }
+    
+  }
+}
+
+function mudaCard6() {
+  let card = document.querySelector('.btn-card6');
+  let button = document.getElementById('btnText6');
+  if (card) {
+    if(card.classList.contains('click')){
+      card.classList.remove('click');
+      button.innerHTML = "+";
+    }else{
+      card.classList.add('click');
+      button.innerHTML = "-";
+    }
+    
+  }
+}
+
+function mudaCard7() {
+  let card = document.querySelector('.btn-card7');
+  let button = document.getElementById('btnText7');
+  if (card) {
+    if(card.classList.contains('click')){
+      card.classList.remove('click');
+      button.innerHTML = "+";
+    }else{
+      card.classList.add('click');
+      button.innerHTML = "-";
+    }
+    
+  }
+}
+
+function mudaCard8() {
+  let card = document.querySelector('.btn-card8');
+  let button = document.getElementById('btnText8');
+  if (card) {
+    if(card.classList.contains('click')){
+      card.classList.remove('click');
+      button.innerHTML = "+";
+    }else{
+      card.classList.add('click');
+      button.innerHTML = "-";
+    }
+    
+  }
+}
+
+function mudaCard9() {
+  let card = document.querySelector('.btn-card9');
+  let button = document.getElementById('btnText9');
+  if (card) {
+    if(card.classList.contains('click')){
+      card.classList.remove('click');
+      button.innerHTML = "+";
+    }else{
+      card.classList.add('click');
+      button.innerHTML = "-";
+    }
+    
+  }
+}
